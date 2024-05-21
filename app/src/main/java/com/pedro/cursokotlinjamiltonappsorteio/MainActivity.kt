ab41c42c19ff56d8219e92acde6040521f95a948
Pedro <pedrohmeprs@gmail.com>
2024-05-21 16:29:22 -0300

    Enviando App Sorteio completo!

---
+++ app/src/main/java/com/pedro/cursokotlinjamiltonappsorteio/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.pedro.cursokotlinjamiltonappsorteio
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun sortear(view:View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numeroSorteado = Random().nextInt(11)
+        textoResultado.setText("Número gerado:$numeroSorteado")
+    }
+}
